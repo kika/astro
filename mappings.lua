3d080c802ec92d4f6de8278c1ca395c3ac9524b8
Kirill Pertsev <kika@kikap.com>
2023-08-07 19:50:09 -0400

    Added Trouble plugin

--- mappings.lua
+++ mappings.lua
@@ -70,6 +70,13 @@ return {
       function() vim.lsp.buf.code_action() end,
       desc = "LSP code action",
     },
+    -- Trouble
+    ["<leader>xx"] = { function() require("trouble").toggle() end},
+    ["<leader>xw"] = { function() require("trouble").open("workspace_diagnostics") end},
+    ["<leader>xd"] = { function() require("trouble").open("document_diagnostics") end},
+    ["<leader>xq"] = { function() require("trouble").open("quickfix") end},
+    ["<leader>xl"] = { function() require("trouble").open("loclist") end},
+    ["gR"] = { function() require("trouble").open("lsp_references") end},
   },
   v = {
     ["<D-v>"] = { '"*p' },
